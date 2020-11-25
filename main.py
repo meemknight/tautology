7447bd6283a2d2fed3818efd8d854116d0e64453
vlod <vladalex420@gmail.com>
2020-11-25 11:29:47 +0200

    made a basic sintax parser

--- main.py
+++ main.py
@@ -1,40 +1,120 @@
 
+exceptionName = "Sintax error"
+operands = "!>&|"
+
 def parseString(s):
     s = str(s)
     s = s.replace(" ", "")
+    s = s.replace("\n", "")
     s = s.replace("->", ">")
     
     for i in s:
-        if i not in ">01&|()" and not i.isalpha():
-            raise Exception("sintax error")
+        if i not in operands + "()" + "01" and not i.isalpha():
+            raise Exception(exceptionName)
     
             
     return s
 
-def parseConstantsOneExpression(s):
+def implication(a, b):
+    if(a == True and b == False):
+        return False
+    return True
+
+def negation(a):
+    return not a
+
+def parseOneExpression(v1, op, v2):
     
+    v1 = True if v1 == '1' else False
+    v2 = True if v2 == '1' else False
+
+    returnVal = ""
+
+    if op == ">":
+        returnVal = implication(v1, v2)
+    elif op == "&":
+        returnVal = v1 and v2
+    elif op == "|":
+        returnVal = v1 or v2
+    else:
+        raise Exception(exceptionName)
+    
+    return "1" if returnVal else "0"
+
+def parseConstantsLinearExpression(s):
+    
+#handle negations
+    while True:
+        if "!0" in s or "!1" in s:
+            s = s.replace("!1", "0")
+            s = s.replace("!0", "1")
+        elif "!" in s:
+            raise Exception(exceptionName)
+        else:
+            break
+
     state = "var1"
-    var1 = " "
+    var1 = "0"
     var2 = " "
     operand = " "
 
     for i in s:
         if state == "var1":
             var1 = i
+            if var1 != '0' and var1 !='1':
+                raise Exception(exceptionName)
+            state = "op"
+
 
         elif state == "var2":
             var2 = i
+            if var2 != '0' and var2 !='1':
+                raise Exception(exceptionName)
+            state = "op"
+            var1 = parseOneExpression(var1, operand, var2)
+
 
         elif state == "op":
             operand = i
+            if operand not in operands:
+                raise Exception(exceptionName)
+            state = "var2"
+
 
         else:  
            raise Exception("")  
 
 
-    return 0
+    return var1
+
+def parseConstantsMoreExpressions(s):
+
+    while "(" in s or ")" in s:
+        pos = 0
+        lastOpen = -1
+
+        for i in s:
+            if i == "(":
+                lastOpen = pos
+            elif i == ")":
+                if lastOpen == -1:
+                    raise Exception(exceptionName)
+                else:
+                    s = s[:lastOpen] + parseConstantsLinearExpression(s[lastOpen+1:pos]) + s[pos+1:]
+                    lastOpen = -1
+                    break
+            pos += 1
+
+        if lastOpen != -1:
+            raise Exception(exceptionName)
+
+    return parseConstantsLinearExpression(s)
 
 
+s  = "(1 -> 1) | ( 1 -> 0)"
 
+s = parseString(s)
 
-print(parseString("a -> b & b -> a "))
+s = parseConstantsMoreExpressions(s)
+
+print(s)
